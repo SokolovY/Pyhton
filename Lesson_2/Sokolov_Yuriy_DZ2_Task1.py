4874a355cc1f3fc52f18722994fcce3fa7e5dc8f
SokolovY <s0k0l0vskiiiy@gmail.com>
2021-08-16 21:41:52 +0500

    Commit

--- Lesson_2/Sokolov_Yuriy_DZ2_Task1.py
+++ Lesson_2/Sokolov_Yuriy_DZ2_Task1.py
@@ -6,7 +6,7 @@ division = 15 / 3
 integer_division = 15 // 2
 degree = 15 ** 2
 
-print((multiplier), type(multiplier))
-print((division), type(division))
-print((integer_division), type(integer_division))
-print((degree), type(degree))
+print( (multiplier), type( multiplier ) )
+print( (division), type( division ) )
+print( (integer_division), type( integer_division ) )
+print( (degree), type( degree ) )
